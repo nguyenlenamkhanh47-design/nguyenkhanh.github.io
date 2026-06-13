e6ac6b064b3ff6a43a0c103968a9fe36ae37b4b5
nguyenlenamkhanh47-design <nguyenlenamkhanh47@gmail.com>
2026-06-13 16:57:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,34 @@ document.body.classList.toggle("dark");
 const topBtn =
 document.getElementById("topBtn");
 
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 300){
+topBtn.style.display = "block";
+}
+else{
+topBtn.style.display = "none";
+}
+
+});
+
 topBtn.addEventListener("click", () => {
+
 window.scrollTo({
 top:0,
 behavior:"smooth"
 });
+
+});
+
+document
+.getElementById("contactForm")
+.addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+alert(
+"Thank you! Your message has been sent."
+);
+
 });
